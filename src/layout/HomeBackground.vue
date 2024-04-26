b2cb065adc1b2af344c960dab692fc594783fe9f
shiYao <shiny7723@gmail.com>
2024-04-26 15:06:11 +0800

    [FIX]調整背景圖片

--- src/layout/HomeBackground.vue
+++ src/layout/HomeBackground.vue
@@ -10,7 +10,7 @@
 <style scoped>
 .layout {
   /* 設置背景圖片 */
-  background-image: url('../assets/home.jpg');
+  background-image: url('../assets/Home.jpg');
   /* 設置背景大小及其他樣式 */
   background-size: cover;
   background-position: center;
