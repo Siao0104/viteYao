5db0163e4a2436af7de737512920777ef48e7d3a
shiYao <shiny7723@gmail.com>
2024-05-24 10:38:06 +0800

    [IMP]調整首頁內容

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -13,14 +13,14 @@
             <template #title>
               <el-icon><setting /></el-icon>設定
             </template>
-            <el-menu-item index="utility1010"> <el-icon><Edit /></el-icon>代碼維護 </el-menu-item>
+            <el-menu-item index="utility1010"> <el-icon><Edit /></el-icon>程式碼維護 </el-menu-item>
           </el-sub-menu>
         </el-menu>
       </el-scrollbar>
     </el-aside>
 
     <el-container>
-      <el-header style="text-align: right; font-size: 12px">
+      <el-header class="header">
         <div class="toolbar">
           <el-dropdown>
             <el-icon class="large-coin">
@@ -32,13 +32,54 @@
               </el-dropdown-menu>
             </template>
           </el-dropdown>
-          <span>{{userName}}</span>
+          <span>{{ userName }}</span>
         </div>
       </el-header>
 
-      <el-main>
+      <el-main class="main">
         <el-scrollbar>
-
+          <div class="top-section">
+            <el-carousel height="150px">
+              <el-carousel-item v-for="(item,index) in images" :key="index">
+                <img :src="item.src" class="carousel-image" @click="externalWebPage(item.url)">
+              </el-carousel-item>
+            </el-carousel>
+          </div>
+          <div class="bottom-section">
+            <div class="left-panel">
+              <el-calendar ref="calendar">
+                <template #header="{ date }">
+                  <span>行事曆</span>
+                  <span>{{ date }}</span>
+                  <el-button-group>
+                    <el-button size="small" @click="selectDate('prev-year')">去年</el-button>
+                    <el-button size="small" @click="selectDate('prev-month')">上個月</el-button>
+                    <el-button size="small" @click="selectDate('today')">今天</el-button>
+                    <el-button size="small" @click="selectDate('next-month')">下個月</el-button>
+                    <el-button size="small" @click="selectDate('next-year')">明年</el-button>
+                  </el-button-group>
+                </template>
+              </el-calendar>
+            </div>
+            <div class="right-panel">
+              <el-scrollbar>
+                <el-table :data="paginatedData">
+                  <el-table-column prop="date" label="Date" width="140" />
+                  <el-table-column prop="name" label="Name" width="120" />
+                  <el-table-column prop="address" label="Address" />
+                </el-table>
+                <div class="pagination-container">
+                  <el-pagination
+                      :current-page="currentPage"
+                      :page-size="pageSize"
+                      :total="tableData.length"
+                      @current-change="handlePageChange"
+                      layout="prev, pager, next"
+                  />
+                </div>
+              </el-scrollbar>
+            </div>
+          </div>
         </el-scrollbar>
       </el-main>
     </el-container>
@@ -47,41 +88,94 @@
 
 <script lang="ts" setup>
 import { Menu as IconMenu, Star, Setting, Edit } from '@element-plus/icons-vue'
-import {CLEAR, GET_USERNAME} from "../store/storeconstants.js";
-import {useRouter} from "vue-router";
-import {useStore} from "vuex";
-import {computed} from "vue";
+import { CLEAR, GET_USERNAME } from "../store/storeconstants.js";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import {computed, ref} from "vue";
+import {CalendarDateType, CalendarInstance} from "element-plus";
+import elementUiImage from '../assets/elementui.png';
+import githubImage from '../assets/github.png';
 
 const router = useRouter();
 const store = useStore();
+const calendar = ref<CalendarInstance>()
 
-const userName = computed(()=> store.getters[`author/${GET_USERNAME}`])
+const tableData = ref([
+  { date: '2016-05-02', name: 'Tom', address: '1' },
+  { date: '2016-05-04', name: 'John', address: '2' },
+  { date: '2016-05-01', name: 'Doe', address: '3' },
+  { date: '2016-05-02', name: 'Tom', address: '4' },
+  { date: '2016-05-04', name: 'John', address: '5' },
+  { date: '2016-05-01', name: 'Doe', address: '6' },
+  { date: '2016-05-02', name: 'Tom', address: '7' },
+  { date: '2016-05-04', name: 'John', address: '8' },
+  { date: '2016-05-01', name: 'Doe', address: '9' },
+  { date: '2016-05-02', name: 'Tom', address: '10' },
+  { date: '2016-05-04', name: 'John', address: '11' },
+  { date: '2016-05-01', name: 'Doe', address: '12' },
+  { date: '2016-05-02', name: 'Tom', address: '13' },
+  { date: '2016-05-04', name: 'John', address: '14' },
+  { date: '2016-05-01', name: 'Doe', address: '15' },
+  { date: '2016-05-01', name: 'Doe', address: '16' }
+]);
 
-//登出
+const currentPage = ref(1);
+const pageSize = ref(12);
+const paginatedData = computed(()=>{
+  const start = (currentPage.value-1)*pageSize.value;
+  const end = start + pageSize.value;
+  return tableData.value.slice(start,end);
+})
+
+const userName = computed(() => store.getters[`author/${GET_USERNAME}`]);
+
+const images = [
+  {
+    src: elementUiImage,
+    url: 'https://element-plus.org/en-US/',
+  },
+  {
+    src: githubImage,
+    url: 'https://github.com/',
+  }
+]
+
 const logOut = () => {
-  //清除store資料
   store.commit(`auth/${CLEAR}`);
-  //清除localStorage資料
   localStorage.clear();
   router.push("/");
-  //防止登出時，瀏覽器返回上一頁還能進入
-  window.addEventListener('popstate',function () {
+  window.addEventListener('popstate', function () {
     window.history.forward();
-  })
+  });
+};
+
+//開啟外部網站
+const externalWebPage = (url) =>{
+  window.open(url,'_blank');
 }
 
+//點選左側菜單開啟TABS
 const handleMenuSelect = (index) => {
-  if(index === 'utility1010'){
-    console.log("測試用!!!")
+  if (index === 'utility1010') {
+    console.log("測試用!!!");
   }
+};
+
+//日曆組件日期選擇
+const selectDate = (val: CalendarDateType) => {
+  if (!calendar.value) return
+  calendar.value.selectDate(val)
 }
 
+const handlePageChange = (page) => {
+  currentPage.value = page;
+}
 </script>
 
 <style scoped>
 .layout-container {
   position: relative;
-  background: url('../assets/maple2.jpg') ;
+  background: url('../assets/maple2.jpg');
   background-size: cover;
 }
 
@@ -92,7 +186,7 @@ const handleMenuSelect = (index) => {
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(255, 255, 255, 0.7); /* 半透明覆蓋層 */
+  background: rgba(255, 255, 255, 0.5);
   pointer-events: none;
 }
 
@@ -100,10 +194,12 @@ const handleMenuSelect = (index) => {
 .layout-container .el-aside,
 .layout-container .el-main {
   position: relative;
-  z-index: 2; /* 確保內容在背景圖片之上 */
+  z-index: 2;
 }
 
 .layout-container .el-header {
+  display: flex;
+  justify-content: flex-end;
   color: var(--el-text-color-primary);
 }
 
@@ -130,5 +226,42 @@ const handleMenuSelect = (index) => {
   font-size: 24px;
   margin-right: 8px;
   margin-top: 1px;
+}
+
+.top-section {
+  height: 20%;
+}
+
+.bottom-section {
+  display: flex;
+  height: 80%;
+}
+
+.left-panel {
+  flex: 2;
+  padding: 10px;
+}
+
+.right-panel {
+  flex: 1;
+  padding: 10px;
+}
+
+.carousel-image {
+  width: auto;
+  height: 100%;
+  max-height: 150px; /* 設置最大高度，保持圖片比例 */
+  cursor: pointer;
+  object-fit: contain; /* 使圖片按比例縮放 */
+}
+
+.el-pagination {
+  text-align: center;
+}
+
+.pagination-container {
+  display: flex;
+  justify-content: center;
+  padding: 20px 0;
 }
 </style>
